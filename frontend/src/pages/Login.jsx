de9ff0b9f0fc934ac00e3fe406980fc1251972fb
atanum62 <atanumaity62899@gmail.com>
2025-04-17 09:10:31 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8080/auth/login", {
+      const response = await fetch("https://deploy-mern-app-1-two.vercel.app/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginInfo),
